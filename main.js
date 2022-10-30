9632bfb1ffdd140f1da3b860ead1b7467fb6efda
Laura <moomal.abro@gmail.com>
2022-10-30 13:31:11 +0500

    most basic functionality complete

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 var days = document.querySelectorAll('.day');
-var today = "2";
+var today = "4";
+var habits = JSON.parse(localStorage.getItem('habits') || "{}");
+if (Object.keys(habits).length) {
+    Object.keys(habits).forEach(function (key) {
+        var habit = document.getElementById(key);
+        if (habit) {
+            var days_1 = Array.from(habit.children);
+            days_1.forEach(function (day) {
+                if (habits[key].includes(day.dataset.day))
+                    day.classList.add('done');
+            });
+        }
+    });
+}
+else {
+    habits = { "habit1": [], "habit2": [], "habit3": [] };
+}
 days.forEach(function (day) {
-    if (day.dataset.day === today) {
+    var date = day.dataset.day;
+    if (parseInt(date) <= parseInt(today)) {
         day.classList.add("today");
         day.addEventListener('click', completeDay);
     }
@@ -10,7 +27,15 @@ days.forEach(function (day) {
     }
 });
 function completeDay() {
-    if (this.dataset.day === today) {
-        this.classList.toggle('done');
+    var habit = this.parentNode.id;
+    var dates = habits[habit];
+    if (dates.includes(this.dataset.day)) {
+        var index = dates.indexOf(this.dataset.day);
+        dates.splice(index, 1);
     }
+    else {
+        dates.push(this.dataset.day);
+    }
+    localStorage.setItem('habits', JSON.stringify(habits));
+    this.classList.toggle('done');
 }
--- main.ts
+++ main.ts
@@ -1,7 +1,27 @@
 const days = document.querySelectorAll('.day') as NodeListOf<HTMLDivElement>;
-const today = "2";
+const today = "4";
+interface Habit {
+	[key: string]: string[];
+}
+let habits: Habit = JSON.parse(localStorage.getItem('habits') || "{}");
+if (Object.keys(habits).length) {
+	Object.keys(habits).forEach(key => {
+	const habit = document.getElementById(key);
+	if (habit) {
+		const days = Array.from(habit.children) as HTMLDivElement[];
+		days.forEach(day => {
+			if (habits[key].includes(day.dataset.day!)) day.classList.add('done');
+		})
+	}
+});
+} else {
+	habits = {"habit1": [], "habit2": [], "habit3": []}
+}
+
+
 days.forEach(day => {
-	if (day.dataset.day === today) {
+	const date = day.dataset.day!;
+	if (parseInt(date) <= parseInt(today)) {
 		day.classList.add("today");
 		day.addEventListener('click', completeDay);
 	} else {
@@ -11,7 +31,14 @@ days.forEach(day => {
 
 
 function completeDay() {
-	if (this.dataset.day === today) {
-		this.classList.toggle('done');
-	}	
+	const habit: string = this.parentNode.id;
+	const dates = habits[habit];
+	if (dates.includes(this.dataset.day)) {
+		const index = dates.indexOf(this.dataset.day);
+		dates.splice(index, 1);
+	} else {
+		dates.push(this.dataset.day);
+	}
+	localStorage.setItem('habits', JSON.stringify(habits));
+	this.classList.toggle('done');
 }
