da841639745f48011c4efb67435e622ce25da9e7
Laura <moomal.abro@gmail.com>
2022-10-31 13:49:02 +0500

    add add habit

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var days = document.querySelectorAll('.day');
+var form = document.getElementById('new-habit-form');
+var habitContainer = document.getElementById('tracker');
 var today = "4";
+form.addEventListener('submit', addHabit);
+habitContainer.addEventListener('click', updateDay);
 var habits = JSON.parse(localStorage.getItem('habits') || "{}");
 if (Object.keys(habits).length) {
     Object.keys(habits).forEach(function (key) {
@@ -20,22 +24,37 @@ days.forEach(function (day) {
     var date = day.dataset.day;
     if (parseInt(date) <= parseInt(today)) {
         day.classList.add("today");
-        day.addEventListener('click', completeDay);
     }
     else {
         day.style.background = '#ccc';
     }
 });
-function completeDay() {
-    var habit = this.parentNode.id;
+function updateDay(e) {
+    var day = e.target;
+    var date = day.dataset.day || "";
+    if (!day.classList.contains("day"))
+        return;
+    var habit = day.parentElement.id;
     var dates = habits[habit];
-    if (dates.includes(this.dataset.day)) {
-        var index = dates.indexOf(this.dataset.day);
+    console.log(day.parentElement);
+    if (dates.includes(date)) {
+        var index = dates.indexOf(date);
         dates.splice(index, 1);
     }
     else {
-        dates.push(this.dataset.day);
+        dates.push(date);
     }
     localStorage.setItem('habits', JSON.stringify(habits));
-    this.classList.toggle('done');
+    day.classList.toggle('done');
+}
+function addHabit(e) {
+    e.preventDefault();
+    var habitName = document.getElementById('new-habit-name').value;
+    if (habitName.length) {
+        var habitId = habitName.toLowerCase().split(" ").join("-");
+        var newHabit = "\n\t\t\t<h2>".concat(habitName, "</h2>\t\t\n\t\t\t<div class=\"habit\" id=\"habit-").concat(habitId, "\">\n\t\t\t\t<div class=\"day day1\" data-day=\"1\"></div>\n\t\t\t\t<div class=\"day day2\" data-day=\"2\"></div>\n\t\t\t\t<div class=\"day day3\" data-day=\"3\"></div>\n\t\t\t\t<div class=\"day day4\" data-day=\"4\"></div>\n\t\t\t\t<div class=\"day day5\" data-day=\"5\"></div>\n\t\t\t</div>");
+        habits["habit-".concat(habitId)] = [];
+        localStorage.setItem('habits', JSON.stringify(habits));
+        habitContainer.innerHTML += newHabit;
+    }
 }
--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
 const days = document.querySelectorAll('.day') as NodeListOf<HTMLDivElement>;
+const form = document.getElementById('new-habit-form') as HTMLFormElement;
+const habitContainer = document.getElementById('tracker') as HTMLDivElement;
+
 const today = "4";
+
+form.addEventListener('submit', addHabit);
+habitContainer.addEventListener('click', updateDay)
+
 interface Habit {
 	[key: string]: string[];
 }
@@ -23,22 +30,45 @@ days.forEach(day => {
 	const date = day.dataset.day!;
 	if (parseInt(date) <= parseInt(today)) {
 		day.classList.add("today");
-		day.addEventListener('click', completeDay);
 	} else {
 		day.style.background = '#ccc';
 	}
 });
 
 
-function completeDay() {
-	const habit: string = this.parentNode.id;
+function updateDay(e: Event) {
+	const day = e.target as HTMLDivElement;
+	const date = day.dataset.day || "";
+	if (!day.classList.contains("day")) return;
+	const habit: string = day.parentElement!.id;
 	const dates = habits[habit];
-	if (dates.includes(this.dataset.day)) {
-		const index = dates.indexOf(this.dataset.day);
+	console.log(day.parentElement)
+	if (dates.includes(date)) {
+		const index = dates.indexOf(date);
 		dates.splice(index, 1);
 	} else {
-		dates.push(this.dataset.day);
+		dates.push(date);
 	}
 	localStorage.setItem('habits', JSON.stringify(habits));
-	this.classList.toggle('done');
+	day.classList.toggle('done');
+}
+
+function addHabit(e: Event) {
+	e.preventDefault();
+	const habitName = (document.getElementById('new-habit-name') as HTMLInputElement).value;
+	if (habitName.length) {
+		const habitId = habitName.toLowerCase().split(" ").join("-");
+		const newHabit = `
+			<h2>${habitName}</h2>		
+			<div class="habit" id="habit-${habitId}">
+				<div class="day day1" data-day="1"></div>
+				<div class="day day2" data-day="2"></div>
+				<div class="day day3" data-day="3"></div>
+				<div class="day day4" data-day="4"></div>
+				<div class="day day5" data-day="5"></div>
+			</div>`;
+		habits[`habit-${habitId}`] = [];
+		localStorage.setItem('habits', JSON.stringify(habits));
+		habitContainer.innerHTML += newHabit;
+	}
 }
