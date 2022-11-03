bb83511391baa2bc8ca51e60829cb1192061217b
Laura <moomal.abro@gmail.com>
2022-11-03 10:00:09 +0500

    udpate tests

--- tests/Habit.test.ts
+++ tests/Habit.test.ts
@@ -4,32 +4,32 @@ import { Habit } from "../src/Habit.js";
 
 describe("Habits", () => {
 	describe('addDate', () => {
-		it('adds a date to an empty array"', () => {
+		it('add a date to an empty array', () => {
 			const habit = new Habit ('Habit');
 			habit.addDate('2022-03-19');
 			expect(habit.dates).toEqual(['2022-03-19']);
 		});
-		it('adds a date to a non-empty array"', () => {
+		it('add a date to a non-empty array', () => {
 			const habit = new Habit ('Habit', ['2022-03-19']);
 			habit.addDate('2022-05-31');
 			expect(habit.dates).toEqual(['2022-03-19', '2022-05-31']);
 		});
-		it('does not add duplicate dates"', () => {
+		it('do not add duplicate dates', () => {
 			const habit = new Habit ('Habit', ['2022-03-19']);
 			habit.addDate('2022-03-19');
 			expect(habit.dates).toEqual(['2022-03-19']);
 		});
-		it('adds dates in sorted order (insert new earliest date)"', () => {
+		it('add dates in sorted order (insert new earliest date)', () => {
 			const habit = new Habit ('Habit', ['2022-03-19']);
 			habit.addDate('2021-03-19');
 			expect(habit.dates).toEqual(['2021-03-19', '2022-03-19']);
 		});
-		it('adds dates in sorted order (insert between existing dates)"', () => {
+		it('add dates in sorted order (insert between existing dates)', () => {
 			const habit = new Habit ('Habit', ['2022-03-19', '2022-09-01']);
 			habit.addDate('2022-05-31');
 			expect(habit.dates).toEqual(['2022-03-19', '2022-05-31', '2022-09-01']);
 		});
-		it('only adds dates in format YYYY-MM-DD"', () => {
+		it('only add dates in format YYYY-MM-DD', () => {
 			const habit = new Habit ('Habit');
 			expect(() => habit.addDate('5')).toThrow('Invalid date');
 			expect(() => habit.addDate('hello')).toThrow('Invalid date');
@@ -37,13 +37,13 @@ describe("Habits", () => {
 		});
 	});
 	describe('removeDate', () => {
-		it('removes date from array"', () => {
+		it('remove date from array', () => {
 			const habit = new Habit ('Habit', ['2022-03-19', '2022-05-31']);
 			habit.removeDate('2022-05-31');
 			expect(habit.dates).toEqual(['2022-03-19']);
 		});
 
-		it('ignores dates if they are not in the array"', () => {
+		it('ignore dates if they are not in the array', () => {
 			const habit = new Habit ('Habit', ['2022-03-19', '2022-05-31']);
 			habit.removeDate('2022-05-31');
 			expect(habit.dates).toEqual(['2022-03-19']);
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -5,11 +5,17 @@ import * as utils from "../src/utils.js";
 
 describe('utils', () => {
 	describe("buildHabits", () => {
-		it('creates habit from localStorage data', () => {
+		it('create single habit from localStorage data', () => {
 			const storage: Habit[] = JSON.parse("[{\"_id\":\"habit-test\",\"_streak\":0,\"name\":\"test\",\"_dates\":[]}]");
-			console.log(storage)
 			expect(utils.buildHabits(storage)).toEqual([new Habit(storage[0].name, storage[0].dates)]);
-			
+		}); 
+		it('create multiple habits from localStorage data', () => {
+			const storage: Habit[] = JSON.parse("[{\"_id\":\"habit-test\",\"_streak\":0,\"name\":\"test\",\"_dates\":[]},{\"_id\":\"habit-habit\",\"_streak\":0,\"name\":\"Habit\",\"_dates\":[]}]");
+			expect(utils.buildHabits(storage)).toEqual([new Habit(storage[0].name, storage[0].dates), new Habit(storage[1].name, storage[1].dates)]);
+		}); 
+		it('handle empty/missing localStorage data', () => {
+			const storage: Habit[] = JSON.parse("[]");
+			expect(utils.buildHabits(storage)).toEqual([]);
 		}); 
 
 	}); 
