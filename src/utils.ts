157056f45793e70aa3d9cf0ecadfd12a4727a51f
Laura <moomal.abro@gmail.com>
2022-11-22 20:47:23 +0500

    prettier reformatting

--- src/utils.ts
+++ src/utils.ts
@@ -1,41 +1,53 @@
-import { Habit } from './habit';
+import { Habit } from "./habit";
+import { DateWithoutTime } from "./DateWithoutTime";
 
 export const buildHabits = (habitData: Habit[]) => {
-	let habits: Habit[] = [];
-	if (habitData.length) {
-		habitData.forEach(data => {
-			const habit = new Habit (data.name, data.dates);
-			habits.push(habit);
-		})
-	}
-	return habits;
-}
+  let habits: Habit[] = [];
+  if (habitData.length) {
+    habitData.forEach((data) => {
+      const habit = new Habit(data.name, data.dates);
+      habits.push(habit);
+    });
+  }
+  return habits;
+};
 
 export const loadHabits = (habits: Habit[], habitContainer: HTMLDivElement) => {
-	if (!habits.length) return;
-	habits.forEach(habit => {
-		const habitEl = `
+  if (!habits.length) return;
+  habits.forEach((habit) => {
+    const habitEl = `
 			<h2>${habit.name}</h2>		
 			<div class="habit" id="habit-${habit.id}">
 			</div>`;
-		habitContainer.innerHTML += habitEl;
-	});
-}
+    habitContainer.innerHTML += habitEl;
+  });
+};
 
-export const addHabit = (habitName: string, habits: Habit[], habitContainer: HTMLDivElement) => {
-	const habit = new Habit(habitName);
-	if (habits.find(h => h.isDuplicate(habitName))) {
-		// TODO: add error message to DOM
-		document.getElementById("message")!.innerText = "This habit already exists.";
-		return;
-	}
-	if (habitName.length) {
-		const newHabit = `
+export const getDates = () => {
+  const today = new DateWithoutTime();
+  const dates = [];
+  for (let i = 0; i < 10; i++) {}
+};
+
+export const addHabit = (
+  habitName: string,
+  habits: Habit[],
+  habitContainer: HTMLDivElement
+) => {
+  const habit = new Habit(habitName);
+  if (habits.find((h) => h.isDuplicate(habitName))) {
+    // TODO: add error message to DOM
+    document.getElementById("message")!.innerText =
+      "This habit already exists.";
+    return;
+  }
+  if (habitName.length) {
+    const newHabit = `
 			<h2>${habit.name}</h2>		
 			<div class="habit" id="${habit.id}">
 			</div>`;
-		habits.push(habit);
-		localStorage.setItem('habits', JSON.stringify(habits));
-		habitContainer.innerHTML += newHabit;
-	}
-}
+    habits.push(habit);
+    localStorage.setItem("habits", JSON.stringify(habits));
+    habitContainer.innerHTML += newHabit;
+  }
+};
